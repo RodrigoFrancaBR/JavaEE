fd79abc99e1fd9c6827b804709b751a715819857
RodrigoFrancaBR <rfrancacosta@gmail.com>
2021-06-20 11:33:37 -0300

    Trabalhando com anotações e não com web.xml;

--- cursoPreparatorio/src/main/java/br/com/franca/web/servlet/HelloWorldServlet.java
+++ cursoPreparatorio/src/main/java/br/com/franca/web/servlet/HelloWorldServlet.java
@@ -1,23 +1,21 @@
 package br.com.franca.web.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloWorldServlet
- */
+@WebServlet(name="HelloWorldServlet", 
+			urlPatterns = "/servlet/helloworld")
 public class HelloWorldServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public HelloWorldServlet() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -26,7 +24,7 @@ public class HelloWorldServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
